f248fed18dcb92eb144501942aa58713bd23b98c
Saurabh Lodha <saurabhlodha11@gmail.com>
2016-01-23 16:12:30 +0530

    Adds scopes for trip model

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,7 +5,7 @@ class TripsController < ApplicationController
 
   def index
     head :unauthorized unless current_user.admin?
-    @trips = Trip.all
+    @trips = filter_trip params
     render json: @trips
   end
 
@@ -36,6 +36,19 @@ class TripsController < ApplicationController
     @trip.destroy
     head :no_content
   end
+
+  protected
+
+    def filter_trip params
+      @trips = Trip.all
+      if params[:q]
+        @trips = @trips.destination(params[:q][:destination]) if params[:q][:destination]
+        @trips = @trips.comment(params[:q][:comment]) if params[:q][:comment]
+        @trips = @trips.starts_after(params[:q][:starts_after]) if params[:q][:starts_after]
+        @trips = @trips.ends_before(params[:q][:ends_before]) if params[:q][:ends_before]
+      end
+      @trips
+    end
 
   private
 
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,8 @@ class Trip < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true
   validates_associated :user
+  scope :destination, -> (destination) { where("lower(destination) like ?", "%#{destination.downcase}%")}
+  scope :comment, -> (comment) { where("lower(comment) like ?", "%#{comment.downcase}%")}
+  scope :starts_after, -> (date) { where("start_date >= ?", date)}
+  scope :ends_before, -> (date) { where("enddate <= ?", date)}
 end
