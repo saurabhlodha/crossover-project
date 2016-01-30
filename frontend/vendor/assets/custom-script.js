01b4fe875f89f7741f326db9315ce857fee45ef6
Saurabh Lodha <saurabhlodha11@gmail.com>
2016-01-30 05:31:05 +0530

    Adds search functionality

--- frontend/vendor/assets/custom-script.js
+++ frontend/vendor/assets/custom-script.js
@@ -3,10 +3,31 @@ $(document).ready(function () {
     $('body').on('keyup', '#filter', function () {
 
         var rex = new RegExp($(this).val(), 'i');
-        $('.searchable tr').hide();
+        var start_date = new Date($(this).val());
+        $('.searchable tr').addClass("search_hide");
         $('.searchable tr').filter(function () {
             return rex.test($(this).text());
-        }).show();
+        }).removeClass("search_hide");
+
+    });
+    $('body').on('change', '#after_start_date', function () {
+
+        var date = new Date($(this).val());
+        console.log(date);
+        $('.searchable tr').addClass("start_date_hide");
+        $('.searchable tr .start_date').filter(function () {
+            return (date < (new Date($(this).text())));
+        }).parent().removeClass("start_date_hide");
+
+    });
+    $('body').on('change', '#before_end_date', function () {
+
+        var date = new Date($(this).val());
+        console.log(date);
+        $('.searchable tr').addClass("end_date_hide");
+        $('.searchable tr .end_date').filter(function () {
+            return (date > (new Date($(this).text())));
+        }).parent().removeClass("end_date_hide");
 
     });
     $('body').on('click', '#print_button', function() {
